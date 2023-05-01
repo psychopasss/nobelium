cd0833e075191cf23bc71ffe74acc9cdd260bf1a
Psycho Pass <x308989414@gmail.com>
2023-05-01 16:16:31 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,8 +1,8 @@
 const BLOG = {
-  title: 'CRAIGARY',
-  author: 'Craig Hart',
-  email: 'zaingg@craigary.net',
-  link: 'https://nobelium.vercel.app',
+  title: "Zain's Blog",
+  author: 'Zain',
+  email: 'zaingg@qq.com',
+  link: 'https://zain.vercel.app',
   description: 'This gonna be an awesome website.',
   lang: 'zh-CN', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
