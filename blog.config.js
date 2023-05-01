66d6f1ae42251ffe865257cdba05bd661a848569
Psycho Pass <x308989414@gmail.com>
2023-05-01 20:47:41 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,9 +1,9 @@
 const BLOG = {
   title: "Zain's Blog",
   author: 'Zain',
-  email: 'zaingg@qq.com',
+  email: 'x308989414@gmail.com',
   link: 'https://blog.izain.cn',
-  description: '',
+  description: 'keep your mind',
   lang: 'zh-CN', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
